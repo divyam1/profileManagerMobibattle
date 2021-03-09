9c9316598178da038e09258254ac9ecc617df610
divyam <divyam.upneja@gmail.com>
2021-03-09 15:32:38 +0530

    filter

--- src/main/java/com/bng/profileManagerMobibattle/ProfileManagerMobibattleApplication.java
+++ src/main/java/com/bng/profileManagerMobibattle/ProfileManagerMobibattleApplication.java
@@ -2,13 +2,37 @@ package com.bng.profileManagerMobibattle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.bng.profileManagerMobibattle.filter.PostLoginAuthorizationFilter;
+import com.bng.profileManagerMobibattle.filter.PreLoginAuthorizationFilter;
+
 
 @SpringBootApplication
 public class ProfileManagerMobibattleApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileManagerMobibattleApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<PreLoginAuthorizationFilter> preLoginFilter(){
+	    FilterRegistrationBean<PreLoginAuthorizationFilter> registrationBean 
+	      = new FilterRegistrationBean<>();
+	    registrationBean.setFilter(new PreLoginAuthorizationFilter());
+	    registrationBean.addUrlPatterns("/prelogin/*");
+	    return registrationBean;    
+	}
+	
+	@Bean
+	public FilterRegistrationBean<PostLoginAuthorizationFilter> postLoginFilter(){
+	    FilterRegistrationBean<PostLoginAuthorizationFilter> registrationBean 
+	      = new FilterRegistrationBean<>();
+	    registrationBean.setFilter(new PostLoginAuthorizationFilter());
+	    registrationBean.addUrlPatterns("/postlogin/*");
+	    return registrationBean;    
 	}
 
 }
